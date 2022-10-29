c75f95a6098bd30d1711e3de4903fa8f875f414a
lesley-byte <phosphorescence3@hotmail.com>
2022-10-29 10:37:36 -0400

    adding different style modes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,11 @@
-// this is where I will put my code
+var mode = "title";
 // using document.createElement() to create a header
 var div_actions = document.createElement("div");
 // using document.createElement() to create a h4 element to put in the header
 var h4 = document.createElement("h4");
 // using document.createElement() to create another h4 element to put in the header
 var h4_2 = document.createElement("h4");
+var h4_3 = document.createElement("h4");
 // using document.createElement() to create a h1 element
 var h1 = document.createElement("h1");
 // using document.createElement() to create a section element
@@ -22,32 +23,11 @@ var button4 = document.createElement("button");
 // using document.createElement() to create a ul element
 // using .textContent to add text to the h1 element
 h1.textContent = "Quiz";
-
-// setting the class attribute of all buttons to "btn"
-button.setAttribute("class", "btn");
-button1.setAttribute("class", "btn");
-button2.setAttribute("class", "btn");
-button3.setAttribute("class", "btn");
-button4.setAttribute("class", "btn");
-// using .setAttribute() to add an id to the form element
-form.setAttribute("id", "form");
-// using .setAttribute() to add an id to the input element
-input.setAttribute("id", "input");
-// using .setAttribute() to add an id to the button element
-button.setAttribute("id", "button");
-// using .setAttribute() to add an id to the button1 element
-button1.setAttribute("id", "button1");
-// using .setAttribute() to add an id to the button2 element
-button2.setAttribute("id", "button2");
-// using .setAttribute() to add an id to the button3 element
-button3.setAttribute("id", "button3");
-// using .setAttribute() to add an id to the button4 element
-button4.setAttribute("id", "button4");
-//using .appendChile to add the header element to the body element
 document.body.appendChild(div_actions);
 // using .appendChild() to add the h4 element to the div_actions element
 div_actions.appendChild(h4);
 div_actions.appendChild(h4_2);
+div_actions.appendChild(h4_3);
 
 // using .appendChild() to add the section to the body element
 document.body.appendChild(section);
@@ -75,6 +55,7 @@ form.appendChild(button3);
 h4.textContent = "Timer: 0";
 // using .textContent to add text to the h4_2 element in the header
 h4_2.textContent = "High Scores";
+h4_3.textContent = "Back";
 
 button.textContent = "Question 1";
 // using .textContent to add text to the button1 element
@@ -87,43 +68,127 @@ button3.textContent = "Question 4";
 // using .textContent to add text to the button4 element
 button4.textContent = "Save";
 
+// setting the class attribute of all buttons to "btn"
+button.setAttribute("class", "btn");
+button1.setAttribute("class", "btn");
+button2.setAttribute("class", "btn");
+button3.setAttribute("class", "btn");
+button4.setAttribute("class", "btn");
+// using .setAttribute() to add an id to the form element
+form.setAttribute("id", "form");
+// using .setAttribute() to add an id to the input element
+input.setAttribute("id", "input");
+// using .setAttribute() to add an id to the button element
+button.setAttribute("id", "button");
+// using .setAttribute() to add an id to the button1 element
+button1.setAttribute("id", "button1");
+// using .setAttribute() to add an id to the button2 element
+button2.setAttribute("id", "button2");
+// using .setAttribute() to add an id to the button3 element
+button3.setAttribute("id", "button3");
+// using .setAttribute() to add an id to the button4 element
+button4.setAttribute("id", "button4");
+//using .appendChile to add the header element to the body element
 // using .setAttribute() to add style to the h1 element
-div_actions.setAttribute(
-  "style",
-  "display:flex; background-color: white; color: black; padding: 20px; text-align: center; flex-direction: row-reverse; width: 95%; justify-content: space-between; margin: 0 auto; border-radius: 5px; margin-bottom: 20px;"
-);
-h1.setAttribute(
-  "style",
-  "text-align:left; width: 100%; color:black; font-size: 50px; font-family: 'Times New Roman', Times, serif;"
-);
-// using .setAttribute() to add style to the section element
-section.setAttribute(
-  "style",
-  "width: 50%; border-bottom: lightgrey 2px solid;margin-bottom: 10px; padding-bottom:10px; text-align:center; color:black; font-size: 50px; font-family: 'Times New Roman', Times, serif; display: flex; flex-direction: column; justify-content: center; align-items: center;"
-);
-// using .setAttribute() to add style to the form element
-form.setAttribute(
-  "style",
-  "text-align:center; width:100%; display:flex; flex-direction:column; color:black; font-size: 50px; font-family: 'Times New Roman', Times, serif;"
-);
-// using .setAttribute() to add style to the input element
-input.setAttribute(
-  "style",
-  "text-align:center; color:blue; font-size: 50px; font-family: 'Times New Roman', Times, serif;"
-);
-// using .setAttribute() to add style to the btn class.all btn class should be purple with white text
-document.querySelectorAll(".btn").forEach((btn) => {
-  btn.setAttribute(
+
+function styleFirstPage() {
+  mode = "first";
+  div_actions.setAttribute(
     "style",
-    "background-color:#5e3d92; border: #5e3d92 1px solid; color:white; margin:5px; border-radius:10px; font-size: 20px; font-family: 'Times New Roman', Times, serif;padding: 0px 10px; width: fit-content;"
+    "display:flex; background-color: white; color: black; padding: 20px; text-align: center; flex-direction: row-reverse; width: 95%; justify-content: space-between; margin: 0 auto; border-radius: 5px; margin-bottom: 20px;"
   );
+  h1.setAttribute(
+    "style",
+    "text-align:left; width: 100%; color:black; font-size: 50px; font-family: 'Times New Roman', Times, serif;"
+  );
+  // using .setAttribute() to add style to the section element
+  section.setAttribute(
+    "style",
+    "width: 50%; border-bottom: lightgrey 2px solid;margin-bottom: 10px; padding-bottom:10px; text-align:center; color:black; font-size: 50px; font-family: 'Times New Roman', Times, serif; display: flex; flex-direction: column; justify-content: center; align-items: center;"
+  );
+  // using .setAttribute() to add style to the form element
+  form.setAttribute(
+    "style",
+    "text-align:center; width:100%; display:flex; flex-direction:column; color:black; font-size: 50px; font-family: 'Times New Roman', Times, serif;"
+  );
+  // using .setAttribute() to add style to the input element
+  input.setAttribute(
+    "style",
+    "text-align:center; color:blue; font-size: 50px; font-family: 'Times New Roman', Times, serif;"
+  );
+  // using .setAttribute() to add style to the btn class.all btn class should be purple with white text
+  document.querySelectorAll(".btn").forEach((btn) => {
+    btn.setAttribute(
+      "style",
+      "background-color:#5e3d92; border: #5e3d92 1px solid; color:white; margin:5px; border-radius:10px; font-size: 20px; font-family: 'Times New Roman', Times, serif;padding: 0px 10px; width: fit-content;"
+    );
+  });
+  //use .setAttribute() to hide button4
+  button4.setAttribute("style", "display:none");
+  // use .setAttribute() to hide the input element
+  input.setAttribute("style", "display:none");
+  h4.textContent = "Timer: 0";
+  h4_2.setAttribute("style", "display:visaible");
+  h4_3.setAttribute("style", "display:none");
+  // using .setAttribute() to add style to the body element
+  document.body.setAttribute(
+    "style",
+    "background-color:white; display:flex; flex-direction:column; justify-content:center; align-items:center;"
+  );
+}
+
+function styleSecondPage() {
+  mode = "second";
+  h4_3.setAttribute("style", "display:visible");
+  // use .setAttribute() to show the input element
+  input.setAttribute("style", "display:visible");
+  // use .setAttribute() to show the button4 element
+  button4.setAttribute(
+    "style",
+    "display:visible; background-color:#5e3d92; border: #5e3d92 1px solid; color:white; margin:5px; border-radius:10px; font-size: 20px; font-family: 'Times New Roman', Times, serif;padding: 0px 10px; width: fit-content;"
+  );
+  button.setAttribute("style", "display:none");
+  button1.setAttribute("style", "display:none");
+  button2.setAttribute("style", "display:none");
+  button3.setAttribute("style", "display:none");
+  h4_2.setAttribute("style", "display:none");
+  // use .setAttribute() to change the text of the button4 element to "Save"
+  button4.textContent = "Save";
+}
+
+function styleTitlePage() {
+  mode = "title";
+  //hide evertthing except h1
+  // use .setAttribute() to hide the input element
+  input.setAttribute("style", "display:none");
+  // use .setAttribute() to hide the button4 element
+  button4.setAttribute("style", "display:none");
+  // use .setAttribute() to hide the button element
+  button.setAttribute("style", "display:none");
+  // use .setAttribute() to hide the button1 element
+  button1.setAttribute("style", "display:none");
+  // use .setAttribute() to hide the button2 element
+  button2.setAttribute("style", "display:none");
+  // use .setAttribute() to hide the button3 element
+  button3.setAttribute("style", "display:none");
+  h4_3.setAttribute("style", "display:none");
+  h4_2.setAttribute("style", "display:visaible");
+  // use .setAttribute() to change the text of the h4_2 element to "View High Scores"
+  h4_2.textContent = "View High Scores";
+  // use .setAttribute() to change the text of the h4 element to "Coding Quiz Challenge"
+  h4.textContent = "Coding Quiz Challenge";
+  h1.addEventListener("click", function () {
+    styleFirstPage();
+  });
+}
+//----------------------------------------------------------------------------------------------------------------
+// if h4_2 is clicked, hide all elements except the input and button4
+h4_3.addEventListener("click", function () {
+  styleTitlePage();
 });
-//use .setAttribute() to hide button4
-button4.setAttribute("style", "display:none");
-// use .setAttribute() to hide the input element
-input.setAttribute("style", "display:none");
-// using .setAttribute() to add style to the body element
-document.body.setAttribute(
-  "style",
-  "background-color:white; display:flex; flex-direction:column; justify-content:center; align-items:center;"
-);
+
+h4_2.addEventListener("click", function () {
+  styleSecondPage();
+  // if h4_2 is clicked again, show all elements except the input and button4
+});
+styleTitlePage();
